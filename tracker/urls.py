3c40d911853f24bbff1e20bbde8e2f3046ae6988
parthfu2 <parthfu2216@gmail.com>
2025-02-16 18:21:30 +0530

    new changes

--- tracker/urls.py
+++ tracker/urls.py
@@ -1,7 +1,16 @@
-from tracker.views import *
 from django.urls import path
+from tracker.views import (
+    index,
+    deleteTransaction,
+    registration,
+    login_page,
+    logout_page,
+)
 
 urlpatterns = [
     path("", index, name="index"),
-    path("delete-Transaction/<uuid>/", deleteTransaction, name="deleteTransaction"),
+    path("registration/", registration, name="registration"),
+    path("login/", login_page, name="login_page"),
+    path("logout/", logout_page, name="logout_page"),
+    path("delete-tranaction/<uuid>", deleteTransaction, name="deleteTransaction"),
 ]
--- tracker/views.py
+++ tracker/views.py
@@ -2,8 +2,71 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from tracker.models import *
 from django.db.models import Sum
+from django.contrib.auth.models import User
+from django.db.models import Q
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 
 
+def registration(request):
+    if request.method == "POST":
+        first_name = request.POST.get("first_name")
+        last_name = request.POST.get("last_name")
+        username = request.POST.get("username")
+        email = request.POST.get("email")
+        password = request.POST.get("password")
+
+        user_obj = User.objects.filter(Q(email=email) | Q(username=username))
+
+        if user_obj.exists():
+            messages.error(request, "Error: Username or Email already exists")
+            return redirect("/registration/")
+
+        user_obj = User.objects.create(
+            first_name=first_name,
+            last_name=last_name,
+            username=username,
+            email=email,
+        )
+
+        user_obj.set_password(password)
+        user_obj.save()
+
+        messages.error(request, "Success: Account Created")
+        return redirect("/registration/")
+
+    return render(request, "registration.html")
+
+
+def login_page(request):
+
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        user_obj = User.objects.filter(username=username)
+        if not user_obj.exists():
+            messages.error(request, "Error: Username does not exist")
+            return redirect("/login/")
+
+        user_obj = authenticate(username=username, password=password)
+
+        if not user_obj:
+            messages.error(request, "Error: Invalid credentials")
+            return redirect("/login/")
+
+        login(request, user_obj)
+        return redirect("/")
+
+    return render(request, "login.html")
+
+
+def logout_page(request):
+    logout(request)
+    messages.error(request, "Success:Logged out successfull")
+    return redirect("/login/")
+
+
+@login_required(login_url="/login/")
 def index(request):
     if request.method == "POST":
         description = request.POST.get("description")
@@ -20,23 +83,32 @@ def index(request):
             return redirect("/")
 
         # Create the transaction after validation
-        Transaction.objects.create(description=description, amount=amount)
+        Transaction.objects.create(
+            description=description, amount=amount, created_by=request.user
+        )
 
         return redirect("/")
 
     # Get all transactions
-    transactions = Transaction.objects.all()
+    transactions = Transaction.objects.filter(created_by=request.user)
 
     # Calculate balance, income, and expenses
     total_balance = (
-        transactions.aggregate(total_balance=Sum("amount"))["total_balance"] or 0
+        transactions.filter(created_by=request.user).aggregate(
+            total_balance=Sum("amount")
+        )["total_balance"]
+        or 0
     )
     income = (
-        transactions.filter(amount__gte=0).aggregate(income=Sum("amount"))["income"]
+        transactions.filter(created_by=request.user, amount__gte=0).aggregate(
+            income=Sum("amount")
+        )["income"]
         or 0
     )
     expense = (
-        transactions.filter(amount__lte=0).aggregate(expense=Sum("amount"))["expense"]
+        transactions.filter(created_by=request.user, amount__lte=0).aggregate(
+            expense=Sum("amount")
+        )["expense"]
         or 0
     )
 
